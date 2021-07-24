4ba74830b39a40af000c31cbf4a69a2c27a22f21
kodiankodes <arthuremmanuel270@gmail.com>
2021-07-24 16:18:15 -0700

    function that produces output according to an int format 'i', and 'd'

---
+++ print_num.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _printf - function that produces output according to format
+ * @format: variable
+ * Return: the number of characters printed (excluding
+ * the null byte used to end output to strings)
+ */
+
+int _printf(const char *format, ...)
+{
+	int j, i = 0, sum = 0;
+	va_list ap;
+	char *p, *sval;
+
+	va_start(ap, format);
+
+	for (j = 0; format[j] != '\0'; j++)
+	{
+		if (*format != '%')
+		{
+			continue;
+		}
+		switch (*p++)
+		{
+		case 'd':
+			ival = va_arg(ap, int);
+			printf("%d", ival);
+			break;
+		case 'i':
+			ival = va_arg(ap, int);
+			printf("%i", ival);
+
+		}
+	}
+	va_end(ap);
+	return (sum);
+}
